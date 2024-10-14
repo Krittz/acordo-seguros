18ed0cfbdd145464140b480e7c887ee666b2752d
Cristian A. Silva <cristianalvessilvak@gmail.com>
2024-10-14 16:43:20 -0300

    Add slider initial

---
+++ dist/public/js/slider.js
@@ -0,0 +1,14 @@
+var swiper = new Swiper(".slider", {
+    spaceBetween: 30,
+    effect: "fade",
+    loop: true,
+    mousewheel: {
+      invert: false
+    },
+  
+    pagination: {
+      el: ".slider-pagination",
+      clickable: true
+    }
+  });
+  
